b128417a803f33c84583f6b690f2e80d6697e15b
migayala <mayalamontoya@gmail.com>
2022-05-29 19:26:23 -0700

    routing pratice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/home';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import ParamsComponent from './components/other';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/home" element={<Welcome />} />
+        <Route path="/:word" element={<ParamsComponent />} />
+        <Route path="/:word/:color/:bgCol" element={<ParamsComponent />}  />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
